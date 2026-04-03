26d6e145f9a99e2363a3784852cb0f16a7859eb0
Shea Martin <shea.martin@gmail.com>
2026-04-02 21:51:05 -0300

    add homebrew to path

--- path.linux.fish
+++ path.linux.fish
@@ -2,4 +2,6 @@ fish_add_path -p /opt/local/bin
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
 fish_add_path ~/.opencode/bin
+fish_add_path /home/linuxbrew/.linuxbrew/bin
+fish_add_path /home/linuxbrew/.linuxbrew/sbin
 fish_add_path /home/linuxbrew/.linuxbrew/opt/libpq/bin

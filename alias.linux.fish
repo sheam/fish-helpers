73c6a9f4a9953b551ca9147dd33932c13e4c937e
Shea Martin <shea.martin@gmail.com>
2026-04-30 13:12:41 -0300

    alias and kitty functionality

--- alias.linux.fish
+++ alias.linux.fish
@@ -7,5 +7,17 @@ function yzd --description 'yazi file browser with directory changing'
     rm -f "$tmp"
 end
 
+function tomd --description 'Convert file to HTML with pandoc'
+    if test (count $argv) -ne 1
+        echo "Usage: tomd <file>"
+        return 1
+    end
+    set -l file $argv[1]
+    set -l name (string replace -r '\.[^.]+$' '' (basename $file))
+    set -l out ~/Documents/$name.html
+    pandoc -s $file -o $out
+    echo $out
+end
+
 abbr -a less 'glow -p'
 abbr -a g glow
--- kitty.linux.fish
+++ kitty.linux.fish
@@ -1,3 +1,17 @@
+# Switch kitty theme in pixi shells, restore on exit
+# PIXI_ENVIRONMENT_NAME is set after fish config runs, so we watch for it
+if test "$TERM" = xterm-kitty
+    function _kitty_pixi_theme --on-variable PIXI_ENVIRONMENT_NAME
+        functions -e _kitty_pixi_theme
+        if set -q PIXI_ENVIRONMENT_NAME
+            kitty @ --to unix:/tmp/kitty-$KITTY_PID set-colors --match id:$KITTY_WINDOW_ID ~/.config/kitty/atelier-sulphur-pool-dark.conf
+            function _kitty_restore_theme --on-event fish_exit
+                kitty @ --to unix:/tmp/kitty-$KITTY_PID set-colors --match id:$KITTY_WINDOW_ID ~/.config/kitty/current-theme.conf
+            end
+        end
+    end
+end
+
 # Switch kitty theme in distrobox containers, restore on exit
 if set -q CONTAINER_ID; and test "$TERM" = xterm-kitty
     kitty @ --to unix:/tmp/kitty-$KITTY_PID set-colors --match id:$KITTY_WINDOW_ID ~/.config/kitty/solarized-dark.conf
